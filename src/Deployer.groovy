e4cacbfb874ecab083521f63c83034f12c36fa1a
saswati24 <45288761+saswati24@users.noreply.github.com>
2022-05-20 19:30:15 +0530

    Update Deployer.groovy

--- src/Deployer.groovy
+++ src/Deployer.groovy
@@ -1,7 +1,8 @@
 @Grapes(
    [  @Grab(group='org.apache.poi', module='poi', version='3.8'),
       @Grab(group='org.apache.poi', module='poi-ooxml', version='3.8'),
-      @Grab(group = 'org.apache.commons', module = 'commons-lang3', version = '3.6')]
+      @Grab(group = 'org.apache.commons', module = 'commons-lang3', version = '3.6'),
+      @Grab(group='org.apache.logging.log4j', module='log4j-core', version='2.17.2')]
 )
 import org.apache.commons.lang3.StringUtils
 import org.apache.poi.ss.usermodel.*
