e0d4f0b77a1753e8fb08fc5de9207e14c9456258
saswati24 <45288761+saswati24@users.noreply.github.com>
2022-05-20 12:37:58 +0530

    Update Deployer.groovy

--- src/Deployer.groovy
+++ src/Deployer.groovy
@@ -88,7 +88,7 @@ class Deployer {
 //        File file = new File(".");
 //        for(String fileNames : file.list()) script.echo(fileNames);
 //         script.echo(script.pwd)
-        def filename = 'C:\Users\jaidi\OneDrive\Documents\GitHub\simple-java-maven-app\temp.xlsx'
+        def filename = 'C:\\Users\\jaidi\\OneDrive\\Documents\\GitHub\\simple-java-maven-app\\temp.xlsx'
 //         GroovyExcelParser parser = new GroovyExcelParser()
         def (headers, rows) = parse(filename)
         println 'Headers'
