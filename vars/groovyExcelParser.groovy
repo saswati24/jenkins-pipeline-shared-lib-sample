d708ac59bacc76ba9fd958c63532d8b9ebc5195d
saswati24 <45288761+saswati24@users.noreply.github.com>
2022-05-20 12:31:38 +0530

    Update groovyExcelParser.groovy

--- vars/groovyExcelParser.groovy
+++ vars/groovyExcelParser.groovy
@@ -1,6 +1,9 @@
 // GroovyExcelParser
 def call(body) {
     echo "Start GroovyExcelParser"
+    
+    git url: "https://github.com/saswati24/simple-java-maven-app.git"
+    sh 'mvn clean install'
 
     new GroovyExcelParser(script:this).run()
 
