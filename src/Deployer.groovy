4622370e43bb1210e899a1ccf2f8bb233f9f4927
saswati24 <45288761+saswati24@users.noreply.github.com>
2022-05-20 11:58:59 +0530

    Update Deployer.groovy

--- src/Deployer.groovy
+++ src/Deployer.groovy
@@ -14,6 +14,95 @@ import java.io.*
 class Deployer {
     int tries = 0
     Script script
+   
+    def parse(path) {
+        InputStream inp = new FileInputStream(path)
+        Workbook wb = WorkbookFactory.create(inp);
+        Sheet sheet = wb.getSheetAt(0);
+
+        Iterator<Row> rowIt = sheet.rowIterator()
+        Row row = rowIt.next()
+        def headers = getRowData(row)
+
+        def rows = []
+        while(rowIt.hasNext()) {
+            row = rowIt.next()
+            rows << getRowData(row)
+        }
+        [headers, rows]
+    }
+
+    def getRowData(Row row) {
+        def data = []
+        for (Cell cell : row) {
+            getValue(row, cell, data)
+        }
+        data
+    }
+
+    def getRowReference(Row row, Cell cell) {
+        def rowIndex = row.getRowNum()
+        def colIndex = cell.getColumnIndex()
+        CellReference ref = new CellReference(rowIndex, colIndex)
+        ref.getRichStringCellValue().getString()
+    }
+
+    def getValue(Row row, Cell cell, List data) {
+        def rowIndex = row.getRowNum()
+        def colIndex = cell.getColumnIndex()
+        def value = ""
+        switch (cell.getCellType()) {
+            case Cell.CELL_TYPE_STRING:
+                value = cell.getRichStringCellValue().getString();
+                break;
+            case Cell.CELL_TYPE_NUMERIC:
+                if (DateUtil.isCellDateFormatted(cell)) {
+                    value = cell.getDateCellValue();
+                } else {
+                    value = cell.getNumericCellValue();
+                }
+                break;
+            case Cell.CELL_TYPE_BOOLEAN:
+                value = cell.getBooleanCellValue();
+                break;
+            case Cell.CELL_TYPE_FORMULA:
+                value = cell.getCellFormula();
+                break;
+            default:
+                value = ""
+        }
+        data[colIndex] = value
+        data
+    }
+
+    def toXml(header, row) {
+        def obj = "<object>\n"
+        row.eachWithIndex { datum, i ->
+            def headerName = header[i]
+            obj += "\t<$headerName>$datum</$headerName>\n"
+        }
+        obj += "</object>"
+    }
+   
+       def runFile() {
+//        File file = new File(".");
+//        for(String fileNames : file.list()) System.out.println(fileNames);
+        def filename = 'temp.xlsx'
+//         GroovyExcelParser parser = new GroovyExcelParser()
+        def (headers, rows) = parse(filename)
+        println 'Headers'
+        println '------------------'
+        headers.each { header ->
+            println header
+        }
+        println "\n"
+        println 'Rows'
+        println '------------------'
+        rows.each { row ->
+            println toXml(headers, row)
+            println row[1]
+        }
+    }
 
     def run() {
         while (tries < 10) {
@@ -21,5 +110,6 @@ class Deployer {
             tries++
             script.echo("tries is numeric: " + StringUtils.isAlphanumeric("" + tries))
         }
+       runFile()
     }
 }
